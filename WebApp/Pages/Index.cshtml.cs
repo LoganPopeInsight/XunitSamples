3fc3d2443dfb21077b6ff08d57f27b8116470371
Logan Pope <Logan.Pope@insight.com>
2020-01-28 11:28:50 -0700

    make some changes

--- WebApp/Pages/Index.cshtml.cs
+++ WebApp/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,19 +13,37 @@ namespace DemoWebApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
         public string Secret { get; set; }
 
         private readonly IConfiguration _configuration = null;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration, IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
             _configuration = configuration;
+            _httpClientFactory = httpClientFactory;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-            Secret = _configuration["AppSecret"];
+            Secret = await GetApiResponse();
         }
+
+        private async Task<string> GetApiResponse()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+           "https://demoapiapplication.azurewebsites.net/api/config");
+            request.Headers.Add("Accept", "application/vnd.github.v3+json");
+            request.Headers.Add("User-Agent", "HttpClientFactory-Sample");
+
+            var client = _httpClientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
     }
 }
